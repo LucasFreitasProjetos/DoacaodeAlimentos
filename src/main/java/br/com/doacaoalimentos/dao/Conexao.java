c59543c187d52671492969e4c84cd95e4a731d51
LucasFreitasProjetos <lucasft00@gmail.com>
2026-05-14 21:06:38 -0300

    Add files via upload
    implementação de classes do projeto

---
+++ src/main/java/br/com/doacaoalimentos/dao/Conexao.java
@@ -0,0 +1,21 @@
+package br.com.doacaoalimentos.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    private static final String URL = "jdbc:mysql://localhost:3306/banco?useSSL=false";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException ex) {
+            throw new RuntimeException("Erro ao conectar ao banco de dados", ex);
+        }
+    }
+}
